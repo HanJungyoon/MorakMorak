2235ec9ba70da245717342d8b6c056f97e909da2
Han Jungyoon <jungyoon0825@hanyang.ac.kr>
2017-11-29 23:11:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hanjungyoon/morakmorak/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.hanjungyoon.morakmorak;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by hanjungyoon on 2017. 11. 29..
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        },2000);
+    }
+}
